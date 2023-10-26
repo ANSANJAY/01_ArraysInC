10b42818f3d6eee4f576d0c048b15cc9692ecfd1
ANSANJAY <anamika.sanjay16@gmail.com>
2023-10-25 21:05:14 -0500

    inc

---
+++ 6_increasing.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+  int n,smallest, a[10],i=0,j=0,temp;
+  printf("Enter the number of elements \n");
+  scanf("%d", &n);
+  printf("enter input \n");
+  for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  printf("Entered array is : \n");
+  for (i = 0; i < n; i++)
+    printf("%d\n", a[i]);
+
+for (i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        if(a[i]<a[j])
+        {
+            //swap
+            temp=a[j];
+            a[j]=a[i];
+            a[i]=temp;
+        }
+    }
+}
+
+printf("array in increasing order\n");
+
+for(i=0;i<n;i++)
+{
+    printf("%d\n",a[i]);
+}
+
+}
