00b1aac3d02af40f25d5e9bf1ecee4afc809bf99
ANSANJAY <anamika.sanjay16@gmail.com>
2023-10-27 05:20:06 -0500

    search

---
+++ 8_linear_search.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i=0,k=0,found=0;
+
+printf("Enter the number of elements \n");
+  scanf("%d", &n);
+      int a[n];
+  printf("enter input \n");
+  for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  printf("Entered array is : \n");
+  for (i = 0; i < n; i++)
+    printf("%d\n", a[i]);
+printf("enter the key \n");
+scanf("%d",&k);
+
+for(i=0;i<n;i++)
+  if(a[i]==k)
+  {
+  printf("key found at %d \n",i+1);
+  found=1;
+  }
+  if(!found)
+  printf("key not found\n");
+
+ return 0;
+}
